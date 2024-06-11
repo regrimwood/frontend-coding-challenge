4f600cb812dd8be721eeb061456cbdcb33a8c0a1
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-11 23:06:33 +1200

    setup basic theming

---
+++ components/Typography.tsx
@@ -0,0 +1,65 @@
+import { PropsWithChildren } from "react";
+
+interface Props {
+  variant?: "h1" | "h2" | "h3" | "h4" | "body1" | "body2" | "button";
+  className?: string;
+}
+
+export default function Typography(props: PropsWithChildren<Props>) {
+  const { variant = "body1", className, children } = props;
+
+  switch (variant) {
+    case "h1":
+      return (
+        <h1
+          className={`${
+            className ?? ""
+          } text-8xl tracking-tighter leading-tight`}
+        >
+          {children}
+        </h1>
+      );
+    case "h2":
+      return (
+        <h2 className={`${className ?? ""} text-4xl tracking-tight`}>
+          {children}
+        </h2>
+      );
+    case "h3":
+      return (
+        <h3 className={`${className ?? ""} text-3xl tracking-tight`}>
+          {children}
+        </h3>
+      );
+    case "h4":
+      return (
+        <h4 className={`${className ?? ""} text-2xl tracking-tight`}>
+          {children}
+        </h4>
+      );
+    case "body1":
+      return (
+        <p className={`${className ?? ""} text-lg tracking-tight`}>
+          {children}
+        </p>
+      );
+    case "body2":
+      return (
+        <p className={`${className ?? ""} text-base tracking-tight`}>
+          {children}
+        </p>
+      );
+    case "button":
+      return (
+        <button className={`${className ?? ""} text-lg tracking-tight`}>
+          {children}
+        </button>
+      );
+    default:
+      return (
+        <p className={`${className ?? ""} text-lg tracking-tight`}>
+          {children}
+        </p>
+      );
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFDFD",
+        lightPurple: "#a9aff4",
+        neonBlue: "#5460EA",
+        purple: "#835EE7",
+        violet: "#2F2351",
+        black: "#08070B",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
