0b16d0df2188bd32b3db042bde98417b2b52cc8b
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-16 11:13:54 +1200

    refactor and add tests for removeGAItemFromCart

--- utils/cart/removeGAItemFromCart.ts
+++ utils/cart/removeGAItemFromCart.ts
@@ -5,36 +5,53 @@ export default function RemoveGAItemFromCart(
   item: TicketInputModel,
   cartItems: GACartModel[]
 ) {
-  const isEventInCart = cartItems.find(
+  const eventInCart = cartItems.find(
     (cartItem) => cartItem.eventId === item.eventId
   );
 
-  const isGAAreaInCart = isEventInCart?.gaAreas.find(
+  const gAAreaInCart = eventInCart?.gaAreas.find(
     (gaArea) => gaArea.gaAreaId === item.gaAreaId
   );
 
-  const isItemInCart = isGAAreaInCart?.tickets.find(
+  const itemInCart = gAAreaInCart?.tickets.find(
     (ticket) => ticket.id === item.priceId
   );
 
-  if (isItemInCart) {
-    return cartItems.map((cartItem) =>
-      cartItem.eventId === item.eventId
-        ? {
-            ...cartItem,
-            gaAreas: cartItem.gaAreas.map((gaArea) =>
-              gaArea.gaAreaId === item.gaAreaId
-                ? {
-                    ...gaArea,
-                    tickets: gaArea.tickets.filter(
-                      (ticket) => ticket.id !== item.priceId
-                    ),
-                  }
-                : gaArea
-            ),
-          }
-        : cartItem
-    );
+  if (eventInCart && gAAreaInCart && itemInCart) {
+    if (eventInCart.gaAreas.length > 1) {
+      if (gAAreaInCart.tickets.length > 1) {
+        return cartItems.map((cartItem) =>
+          cartItem.eventId === item.eventId
+            ? {
+                ...cartItem,
+                gaAreas: cartItem.gaAreas.map((gaArea) =>
+                  gaArea.gaAreaId === item.gaAreaId
+                    ? {
+                        ...gaArea,
+                        tickets: gaArea.tickets.filter(
+                          (ticket) => ticket.id !== item.priceId
+                        ),
+                      }
+                    : gaArea
+                ),
+              }
+            : cartItem
+        );
+      } else {
+        return cartItems.map((cartItem) =>
+          cartItem.eventId === item.eventId
+            ? {
+                ...cartItem,
+                gaAreas: cartItem.gaAreas.filter(
+                  (gaArea) => gaArea.gaAreaId !== item.gaAreaId
+                ),
+              }
+            : cartItem
+        );
+      }
+    } else {
+      return cartItems.filter((cartItem) => cartItem.eventId !== item.eventId);
+    }
   } else {
     throw new Error(CartErrorEnum.ITEM_NOT_FOUND);
   }
