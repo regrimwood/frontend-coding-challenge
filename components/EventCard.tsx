d3e56ddb115ae6a9aac78b94e14f99d143472c53
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-15 16:31:45 +1200

    add hover animation

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { EventBriefModel } from "../utils/models/EventModel";
 import { useState } from "react";
+import { motion } from "framer-motion";
+import { EventBriefModel } from "../utils/models/EventModel";
 import tailwindConfig from "../tailwind.config";
 import Typography from "./Typography";
 
@@ -12,11 +13,29 @@ export default function EventCard({ event }: { event: EventBriefModel }) {
 
   const { md, lg, xl } = tailwindConfig.theme.screens;
 
+  const variants = {
+    initial: {
+      scale: 1,
+      boxShadow: "0px 5px 10px 2px rgba(0,0,0,0.1)",
+    },
+    hover: {
+      scale: 1.02,
+      boxShadow: "0px 10px 15px 0px rgba(0,0,0,0.2)",
+      transition: { type: "tween", duration: 0.25 },
+    },
+  };
+
   return (
-    <div className="bg-white shadow-lg rounded-xl overflow-hidden h-full">
+    <motion.div
+      className="bg-white  rounded-xl overflow-hidden h-full"
+      initial="initial"
+      whileHover="hover"
+      variants={variants}
+      layout
+    >
       <Link href="#" className="cursor-pointer">
         <div className="h-full cursor-pointer">
-          <div className="relative aspect-[4/5] w-full">
+          <div className="relative aspect-[4/5] w-full overflow-hidden">
             <Image
               className="absolute"
               src={imageError ? "/iTICKET.svg" : imageUrl}
@@ -34,6 +53,6 @@ export default function EventCard({ event }: { event: EventBriefModel }) {
           </div>
         </div>
       </Link>
-    </div>
+    </motion.div>
   );
 }
