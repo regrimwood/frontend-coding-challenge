ea8762010dc940970a2234211b3201a40aae38ce
Rachel Grimwood <85718875+regrimwood@users.noreply.github.com>
2024-06-15 18:51:25 +1200

    add button and color utils

---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import tailwindConfig from "tailwind.config";
+import alpha from "utils/alpha";
+
+export default function Button({
+  children,
+  onClick,
+  className,
+  selected,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  selected?: boolean;
+}) {
+  const { neonBlue, white } = tailwindConfig.theme.extend.colors;
+
+  const variants = {
+    unselected: {
+      backgroundColor: alpha(neonBlue, 0),
+      color: neonBlue,
+    },
+    selected: {
+      backgroundColor: neonBlue,
+      color: white,
+    },
+  };
+
+  return (
+    <motion.button
+      onClick={onClick}
+      className={`font-medium py-2 px-6 rounded-md border-neonBlue border-2 ${className}`}
+      initial="unselected"
+      whileHover="selected"
+      animate={selected ? "selected" : "unselected"}
+      variants={variants}
+    >
+      {children}
+    </motion.button>
+  );
+}
